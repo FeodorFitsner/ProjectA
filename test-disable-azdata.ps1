93a061775b5dfccee385d5222f260facd18f670a
Ilya Finkelshteyn <ilyaf@appveyor.com>
2017-10-17 12:13:22 -0700

    Create test-disable-azdata.ps1

---
+++ test-disable-azdata.ps1
@@ -0,0 +1,4 @@
+function Disable-AzureDataCollectionSilently() {
+    (Disable-AzureDataCollection -WarningAction SilentlyContinue) | Out-Null # To avoid question about enabling data collection when Azure modules are invoked
+}
+Disable-AzureDataCollectionSilently
