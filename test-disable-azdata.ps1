2062b9f4aa0edf19e15ccc35e27de2dd6a441f44
Ilya Finkelshteyn <ilyaf@appveyor.com>
2017-10-17 14:29:39 -0700

    Update test-disable-azdata.ps1

--- test-disable-azdata.ps1
+++ test-disable-azdata.ps1
@@ -1,5 +1,7 @@
 write-host "inside test-disable-azdata.ps1"
 function Disable-AzureDataCollectionSilently() {
+    write-host "inside function start"
     (Disable-AzureDataCollection -WarningAction SilentlyContinue) | Out-Null # To avoid question about enabling data collection when Azure modules are invoked
+    write-host "inside function end"
 }
 Disable-AzureDataCollectionSilently
